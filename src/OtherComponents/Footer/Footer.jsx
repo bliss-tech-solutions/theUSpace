a915f35a1499e62f2ac9f80769866b18d1ec524f
webtesting22 <developer.bliss@gmail.com>
2025-10-03 18:32:32 +0530

    Updated under working modal on website

--- src/OtherComponents/Footer/Footer.jsx
+++ src/OtherComponents/Footer/Footer.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
+import { Modal } from "antd";
 
 const Footer = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleLinkClick = (e) => {
+        e.preventDefault();
+        setModalOpen(true);
+    };
+
     return (
         <div className="Footer PaddingSectionTop100 MarginTitleTop80" id="Footer">
             <div className="Container">
                 <div className="FooterContent">
                     <div className="FooterBlock">
-                        <div>
-                            <h4>ADDRESS</h4>
-                            <p>2972 Westheimer Rd. Santa Ana,</p>
-                            <p>Illinois 85486</p>
+                        <div className="FooterLogoContainer">
+                            <img src="/Images/TheUSpaceWhiteLogo.png" alt="" />
                         </div>
                         <div>
-                            <h4>CONTACT US</h4>
-                            <p>+(378) 555-0108</p>
-                            <p>info@example.io</p>
+                            <div>
+                                <h4>ADDRESS</h4>
+                                <p>2972 Westheimer Rd. Santa Ana,</p>
+                                <p>Illinois 85486</p>
+                            </div>
+                            <div>
+                                <h4>CONTACT US</h4>
+                                <p>+(378) 555-0108</p>
+                                <p>info@example.io</p>
+                            </div>
                         </div>
                     </div>
                     {/* <div className="FooterBlock">
@@ -24,30 +37,30 @@ const Footer = () => {
                     <div className="FooterBlock">
                         <h4>PAGES</h4>
                         <ul>
-                            <li><a href="#AboutHome">About us</a></li>
-                            <li><a href="#OurProductsDesignsHome">Projects</a></li>
-                            <li><a href="#Services">Services</a></li>
-                            <li><a href="#Blogs">Blogs</a></li>
-                            <li><a href="#Contact">Contact us</a></li>
+                            <li><a href="#AboutHome" onClick={handleLinkClick}>About us</a></li>
+                            <li><a href="#OurProductsDesignsHome" onClick={handleLinkClick}>Projects</a></li>
+                            <li><a href="#Services" onClick={handleLinkClick}>Services</a></li>
+                            <li><a href="#Blogs" onClick={handleLinkClick}>Blogs</a></li>
+                            <li><a href="#Contact" onClick={handleLinkClick}>Contact us</a></li>
                         </ul>
                     </div>
                     <div className="FooterBlock">
                         <h4>RESOURCES</h4>
                         <ul>
-                            <li><a href="#">Style Guide</a></li>
-                            <li><a href="#">License</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Cookie Policy</a></li>
-                            <li><a href="#">Terms & Conditions</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Style Guide</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>License</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Privacy Policy</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Cookie Policy</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Terms & Conditions</a></li>
                         </ul>
                     </div>
                     <div className="FooterBlock">
                         <h4>FOLLOW</h4>
                         <ul>
-                            <li><a href="#">Instagram</a></li>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">YouTube</a></li>
-                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Instagram</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Facebook</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>YouTube</a></li>
+                            <li><a href="#" onClick={handleLinkClick}>Twitter</a></li>
                         </ul>
                     </div>
 
@@ -65,6 +78,28 @@ const Footer = () => {
                     </div> */}
                 </div>
             </div>
+
+            {/* Under Working Modal */}
+            <Modal
+                title="Under Development"
+                open={modalOpen}
+                onCancel={() => setModalOpen(false)}
+                footer={null}
+                centered
+                width={400}
+            >
+                <div style={{ textAlign: 'center', padding: '20px 0' }}>
+                    <div style={{ fontSize: '48px', marginBottom: '20px' }}>🚧</div>
+                    <h3 style={{ marginBottom: '15px', color: '#333' }}>Coming Soon</h3>
+                    <p style={{ color: '#666', lineHeight: '1.6' }}>
+                        This feature is currently under development.
+                        We're working hard to bring you the best experience.
+                    </p>
+                    <p style={{ color: '#999', fontSize: '14px', marginTop: '15px' }}>
+                        Thank you for your patience!
+                    </p>
+                </div>
+            </Modal>
         </div>
     );
 };
--- src/OtherComponents/NavigationBar/NavigationBar.jsx
+++ src/OtherComponents/NavigationBar/NavigationBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./NavigationBar.css";
 import NavigationLinksData from "./NavigationLinksData";
 import { Link } from "react-router-dom";
-import { Drawer } from "antd";
+import { Drawer, Modal } from "antd";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
 const NavigationBar = () => {
@@ -37,6 +37,12 @@ const NavigationBar = () => {
     }, []);
 
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleLinkClick = (e) => {
+        e.preventDefault();
+        setModalOpen(true);
+    };
 
     return (
         <div className="Container" id="NavigationBar">
@@ -63,7 +69,7 @@ const NavigationBar = () => {
                     <div>
                         {NavigationLinksData.map((item) => (
                             <div key={item.id}>
-                                <Link to={item.link}>{item.name}</Link>
+                                <Link to={item.link} onClick={handleLinkClick}>{item.name}</Link>
                             </div>
                         ))}
                     </div>
@@ -94,12 +100,37 @@ const NavigationBar = () => {
                 </div>
                 <div className="DrawerLinks">
                     {NavigationLinksData.map((item) => (
-                        <Link key={item.id} to={item.link} onClick={() => setDrawerOpen(false)}>
+                        <Link key={item.id} to={item.link} onClick={(e) => {
+                            handleLinkClick(e);
+                            setDrawerOpen(false);
+                        }}>
                             {item.name}
                         </Link>
                     ))}
                 </div>
             </Drawer>
+
+            {/* Under Working Modal */}
+            <Modal
+                title="Under Development"
+                open={modalOpen}
+                onCancel={() => setModalOpen(false)}
+                footer={null}
+                centered
+                width={400}
+            >
+                <div style={{ textAlign: 'center', padding: '20px 0' }}>
+                    <div style={{ fontSize: '48px', marginBottom: '20px' }}>🚧</div>
+                    <h3 style={{ marginBottom: '15px', color: '#333' }}>Coming Soon</h3>
+                    <p style={{ color: '#666', lineHeight: '1.6' }}>
+                        This feature is currently under development. 
+                        We're working hard to bring you the best experience.
+                    </p>
+                    <p style={{ color: '#999', fontSize: '14px', marginTop: '15px' }}>
+                        Thank you for your patience!
+                    </p>
+                </div>
+            </Modal>
         </div>
     )
 }
